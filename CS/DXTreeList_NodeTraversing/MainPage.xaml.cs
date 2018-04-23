1cb1dfa0c1c8833f090ccdc8ab697175f167bc3e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXTreeList_NodeTraversing/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Grid;
+
+namespace DXTreeList_NodeTraversing {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            treeListControl1.ItemsSource = Stuff.GetStuff();
+        }
+
+        private void treeListControl1_Loaded(object sender, RoutedEventArgs e) {
+            SmartExpandNodes(4);
+        }
+
+        private void SmartExpandNodes(int minChildCount) {
+            TreeListNodeIterator nodeIterator = new TreeListNodeIterator(treeListView1.Nodes, true);
+            while (nodeIterator.MoveNext())
+                nodeIterator.Current.IsExpanded = nodeIterator.Current.Nodes.Count >= minChildCount;
+        }
+    }
+}
